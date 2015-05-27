7e2e09ad7a5cfe4ce7100118c70c8ec7ec7cc957
Steve Kinney <hello@stevekinney.net>
2015-05-27 10:15:14 -0400

    Implement basic functionality

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import styles from './stylesheets/main';
 
 import $ from 'jquery';
+import context from './lib/audio-context';
 
-let oscillatorOn = false;
+const oscillator = context.createOscillator();
+const gain = context.createGain();
+
+oscillator.frequency.value = 220;
+gain.gain.value = 0;
+
+oscillator.connect(gain);
+gain.connect(context.destination);
+oscillator.start(0);
 
 $('#frequency').on('change, keyup', function () {
-  console.log(this.value);
+  oscillator.frequency.value = parseInt(this.value, 10);
 });
 
 $('.type').on('change', function () {
-  console.log(this.value);
+  oscillator.type = this.value;
 });
 
-$('#start-stop').on('click', function () {
-  oscillatorOn = !oscillatorOn;
-  console.log('wowowow');
-  if (oscillatorOn) {
-    $(this).addClass('on').text('Stop');
-  } else {
-    $(this).removeClass('on').text('Start');
-  }
+$('#gain-value').on('change', function () {
+  gain.gain.value = parseFloat(this.value, 10);
 });
