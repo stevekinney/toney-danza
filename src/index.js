5c1cc80b75fa3d4afbd9b343dd951a3c7eb3ba76
Steve Kinney <hello@stevekinney.net>
2015-05-26 23:25:42 -0400

    Get basic UI set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-require('./stylesheets/main');
+import styles from './stylesheets/main';
+
+import $ from 'jquery';
+
+let oscillatorOn = false;
+
+$('#frequency').on('change, keyup', function () {
+  console.log(this.value);
+});
+
+$('.type').on('change', function () {
+  console.log(this.value);
+});
+
+$('#start-stop').on('click', function () {
+  oscillatorOn = !oscillatorOn;
+  console.log('wowowow');
+  if (oscillatorOn) {
+    $(this).addClass('on').text('Stop');
+  } else {
+    $(this).removeClass('on').text('Start');
+  }
+});
